e5be0bb4a17038c9758b980ad7a7a0f20832b2f3
Sebastian Nobbelin <sebastiannobbelin@gmail.com>
2025-03-05 01:38:26 +0100

    more vocs

--- src/lib/players.ts
+++ src/lib/players.ts
@@ -1,250 +1,275 @@
-import type { PlayerSettings } from '@prisma/client';
+import type {PlayerSettings} from '@prisma/client';
 
-import type { GuildMembership } from '$lib/guilds';
-import type { Outfit } from '$lib/outfits';
-import { $_ } from '$lib/utils';
+import type {GuildMembership} from '$lib/guilds';
+import type {Outfit} from '$lib/outfits';
+import {$_} from '$lib/utils';
 
 export type Skills = {
-	magic: number;
-	fist: number;
-	club: number;
-	sword: number;
-	axe: number;
-	distance: number;
-	shielding: number;
-	fishing: number;
+    magic: number;
+    fist: number;
+    club: number;
+    sword: number;
+    axe: number;
+    distance: number;
+    shielding: number;
+    fishing: number;
 };
 
 export type Player = {
-	id: number;
-	name: string;
-	level: number;
-	experience?: bigint;
-	balance?: bigint;
-	vocation: PlayerVocation;
-	sex: PlayerSex;
-	pronoun: PlayerPronoun;
-	online: boolean;
-	deletion: Date | null;
-	isMain: boolean;
-	townName: string | null;
-	lastLogin: Date | null;
+    id: number;
+    name: string;
+    level: number;
+    experience?: bigint;
+    balance?: bigint;
+    vocation: PlayerVocation;
+    sex: PlayerSex;
+    pronoun: PlayerPronoun;
+    online: boolean;
+    deletion: Date | null;
+    isMain: boolean;
+    townName: string | null;
+    lastLogin: Date | null;
 
-	guild?: GuildMembership | null;
-	guildInvtes: string[];
+    guild?: GuildMembership | null;
+    guildInvtes: string[];
 
-	settings?: PlayerSettings;
+    settings?: PlayerSettings;
 } & Outfit;
 
 export type PlayerWithRank = Player & {
-	rank: number;
-	skill: string;
+    rank: number;
+    skill: string;
 };
 
 export enum PlayerSex {
-	Female = 0,
-	Male,
+    Female = 0,
+    Male,
 }
 
 export enum PlayerPronoun {
-	Unset = 0,
-	They,
-	She,
-	He,
-	Ze,
-	Name,
+    Unset = 0,
+    They,
+    She,
+    He,
+    Ze,
+    Name,
 }
 
 export enum PlayerVocation {
-	None = 0,
-	Sorcerer,
-	Druid,
-	Paladin,
-	Knight,
-	MasterSorcerer,
-	ElderDruid,
-	RoyalPaladin,
-	EliteKnight,
+    None = 0,
+    Sorcerer,
+    Druid,
+    Paladin,
+    Knight,
+    MasterSorcerer,
+    ElderDruid,
+    RoyalPaladin,
+    EliteKnight,
+    HeroicKnight=20,
+    LegendaryKnight,
+    HeroicPaladin,
+    LegendaryPaladin,
+    HeroicDruid,
+    LegendaryDruid,
+    HeroicSorcerer,
+    LegendarySorcerer,
+
 }
 
 export function vocationIds(vocation: string): PlayerVocation[] {
-	switch (vocation.toLowerCase()) {
-		case 'none':
-			return [PlayerVocation.None];
-		case 'sorcerer':
-			return [PlayerVocation.Sorcerer, PlayerVocation.MasterSorcerer];
-		case 'druid':
-			return [PlayerVocation.Druid, PlayerVocation.ElderDruid];
-		case 'paladin':
-			return [PlayerVocation.Paladin, PlayerVocation.RoyalPaladin];
-		case 'knight':
-			return [PlayerVocation.Knight, PlayerVocation.EliteKnight];
-		default:
-			return [];
-	}
+    switch (vocation.toLowerCase()) {
+        case 'none':
+            return [PlayerVocation.None];
+        case 'sorcerer':
+            return [PlayerVocation.Sorcerer, PlayerVocation.MasterSorcerer, PlayerVocation.HeroicSorcerer, PlayerVocation.LegendarySorcerer];
+        case 'druid':
+            return [PlayerVocation.Druid, PlayerVocation.ElderDruid, PlayerVocation.HeroicDruid, PlayerVocation.LegendaryDruid];
+        case 'paladin':
+            return [PlayerVocation.Paladin, PlayerVocation.RoyalPaladin, PlayerVocation.HeroicPaladin, PlayerVocation.LegendaryPaladin];
+        case 'knight':
+            return [PlayerVocation.Knight, PlayerVocation.EliteKnight, PlayerVocation.HeroicKnight, PlayerVocation.LegendaryKnight];
+        default:
+            return [];
+    }
 }
 
 export enum PlayerGroup {
-	Normal = 1,
-	Tutor,
-	Seniortutor,
-	Gamemaster,
-	Communitymanager,
-	God,
+    Normal = 1,
+    Tutor,
+    Seniortutor,
+    Gamemaster,
+    Communitymanager,
+    God,
 }
 
 export const allPronouns = [
-	PlayerPronoun.Unset,
-	PlayerPronoun.They,
-	PlayerPronoun.She,
-	PlayerPronoun.He,
-	PlayerPronoun.Ze,
-	PlayerPronoun.Name,
+    PlayerPronoun.Unset,
+    PlayerPronoun.They,
+    PlayerPronoun.She,
+    PlayerPronoun.He,
+    PlayerPronoun.Ze,
+    PlayerPronoun.Name,
 ];
 
 export function parsePlayerSex(value: unknown): PlayerSex {
-	if (typeof value === 'string') {
-		value = parseInt(value);
-	}
-	if (isPlayerSex(value)) {
-		return value;
-	}
-	return PlayerSex.Female;
+    if (typeof value === 'string') {
+        value = parseInt(value);
+    }
+    if (isPlayerSex(value)) {
+        return value;
+    }
+    return PlayerSex.Female;
 }
 
 export function parsePlayerPronoun(value: unknown): PlayerPronoun {
-	if (typeof value === 'string') {
-		value = parseInt(value);
-	}
-	if (isPlayerPronoun(value)) {
-		return value;
-	}
-	return PlayerPronoun.Unset;
+    if (typeof value === 'string') {
+        value = parseInt(value);
+    }
+    if (isPlayerPronoun(value)) {
+        return value;
+    }
+    return PlayerPronoun.Unset;
 }
 
 export function isPlayerSex(value: unknown): value is PlayerSex {
-	if (typeof value != 'number') {
-		return false;
-	}
-	const sex = value as PlayerSex;
-	return sex === PlayerSex.Female || sex === PlayerSex.Male;
+    if (typeof value != 'number') {
+        return false;
+    }
+    const sex = value as PlayerSex;
+    return sex === PlayerSex.Female || sex === PlayerSex.Male;
 }
 
 export function isPlayerPronoun(value: unknown): value is PlayerPronoun {
-	if (typeof value != 'number') {
-		return false;
-	}
-	const pronoun = value as PlayerPronoun;
-	return (
-		pronoun === PlayerPronoun.Unset ||
-		pronoun === PlayerPronoun.They ||
-		pronoun === PlayerPronoun.She ||
-		pronoun === PlayerPronoun.He ||
-		pronoun === PlayerPronoun.Ze ||
-		pronoun === PlayerPronoun.Name
-	);
+    if (typeof value != 'number') {
+        return false;
+    }
+    const pronoun = value as PlayerPronoun;
+    return (
+        pronoun === PlayerPronoun.Unset ||
+        pronoun === PlayerPronoun.They ||
+        pronoun === PlayerPronoun.She ||
+        pronoun === PlayerPronoun.He ||
+        pronoun === PlayerPronoun.Ze ||
+        pronoun === PlayerPronoun.Name
+    );
 }
 
 export function isPlayerVocation(value: unknown): value is PlayerVocation {
-	if (typeof value != 'number') {
-		return false;
-	}
-	const vocation = value as PlayerVocation;
-	return (
-		vocation === PlayerVocation.None ||
-		vocation === PlayerVocation.Sorcerer ||
-		vocation === PlayerVocation.Druid ||
-		vocation === PlayerVocation.Paladin ||
-		vocation === PlayerVocation.Knight ||
-		vocation === PlayerVocation.MasterSorcerer ||
-		vocation === PlayerVocation.ElderDruid ||
-		vocation === PlayerVocation.RoyalPaladin ||
-		vocation === PlayerVocation.EliteKnight
-	);
+    if (typeof value != 'number') {
+        return false;
+    }
+    const vocation = value as PlayerVocation;
+    return (
+        vocation === PlayerVocation.None ||
+        vocation === PlayerVocation.Sorcerer ||
+        vocation === PlayerVocation.Druid ||
+        vocation === PlayerVocation.Paladin ||
+        vocation === PlayerVocation.Knight ||
+        vocation === PlayerVocation.MasterSorcerer ||
+        vocation === PlayerVocation.ElderDruid ||
+        vocation === PlayerVocation.RoyalPaladin ||
+        vocation === PlayerVocation.EliteKnight ||
+        vocation === PlayerVocation.HeroicSorcerer ||
+        vocation === PlayerVocation.HeroicDruid ||
+        vocation === PlayerVocation.HeroicPaladin ||
+        vocation === PlayerVocation.HeroicKnight ||
+        vocation === PlayerVocation.LegendarySorcerer ||
+        vocation === PlayerVocation.LegendaryDruid ||
+        vocation === PlayerVocation.LegendaryPaladin ||
+        vocation === PlayerVocation.LegendaryKnight
+    );
 }
 
 export function sexString(playerSex: PlayerSex): string {
-	return {
-		[PlayerSex.Female]: $_('sexes.female'),
-		[PlayerSex.Male]: $_('sexes.male'),
-	}[playerSex];
+    return {
+        [PlayerSex.Female]: $_('sexes.female'),
+        [PlayerSex.Male]: $_('sexes.male'),
+    }[playerSex];
 }
 
 export function pronounString(playerPronoun: PlayerPronoun): string {
-	return {
-		[PlayerPronoun.Unset]: $_('pronouns.unset'),
-		[PlayerPronoun.They]: $_('pronouns.they'),
-		[PlayerPronoun.She]: $_('pronouns.she'),
-		[PlayerPronoun.He]: $_('pronouns.he'),
-		[PlayerPronoun.Ze]: $_('pronouns.ze'),
-		[PlayerPronoun.Name]: $_('pronouns.name'),
-	}[playerPronoun];
+    return {
+        [PlayerPronoun.Unset]: $_('pronouns.unset'),
+        [PlayerPronoun.They]: $_('pronouns.they'),
+        [PlayerPronoun.She]: $_('pronouns.she'),
+        [PlayerPronoun.He]: $_('pronouns.he'),
+        [PlayerPronoun.Ze]: $_('pronouns.ze'),
+        [PlayerPronoun.Name]: $_('pronouns.name'),
+    }[playerPronoun];
 }
 
 export function vocationString(playerVocation: PlayerVocation): string {
-	return {
-		[PlayerVocation.None]: $_('vocations.none'),
-		[PlayerVocation.Sorcerer]: $_('vocations.sorcerer'),
-		[PlayerVocation.Druid]: $_('vocations.druid'),
-		[PlayerVocation.Paladin]: $_('vocations.paladin'),
-		[PlayerVocation.Knight]: $_('vocations.knight'),
-		[PlayerVocation.MasterSorcerer]: $_('vocations.master-sorcerer'),
-		[PlayerVocation.ElderDruid]: $_('vocations.elder-druid'),
-		[PlayerVocation.RoyalPaladin]: $_('vocations.royal-paladin'),
-		[PlayerVocation.EliteKnight]: $_('vocations.elite-knight'),
-	}[playerVocation];
+    return {
+        [PlayerVocation.None]: $_('vocations.none'),
+        [PlayerVocation.Sorcerer]: $_('vocations.sorcerer'),
+        [PlayerVocation.Druid]: $_('vocations.druid'),
+        [PlayerVocation.Paladin]: $_('vocations.paladin'),
+        [PlayerVocation.Knight]: $_('vocations.knight'),
+        [PlayerVocation.MasterSorcerer]: $_('vocations.master-sorcerer'),
+        [PlayerVocation.ElderDruid]: $_('vocations.elder-druid'),
+        [PlayerVocation.RoyalPaladin]: $_('vocations.royal-paladin'),
+        [PlayerVocation.EliteKnight]: $_('vocations.elite-knight'),
+        [PlayerVocation.HeroicSorcerer]: $_('vocations.heroic-sorcerer'),
+        [PlayerVocation.HeroicDruid]: $_('vocations.heroic-druid'),
+        [PlayerVocation.HeroicPaladin]: $_('vocations.heroic-paladin'),
+        [PlayerVocation.HeroicKnight]: $_('vocations.heroic-knight'),
+        [PlayerVocation.LegendarySorcerer]: $_('vocations.legendary-sorcerer'),
+        [PlayerVocation.LegendaryDruid]: $_('vocations.legendary-druid'),
+        [PlayerVocation.LegendaryPaladin]: $_('vocations.legendary-paladin'),
+        [PlayerVocation.LegendaryKnight]: $_('vocations.legendary-knight'),
+    }[playerVocation];
 }
 
 const validOutfitKeys = [
-	'looktype',
-	'lookaddons',
-	'lookhead',
-	'lookbody',
-	'looklegs',
-	'lookfeet',
-	'mount',
-	'resize',
+    'looktype',
+    'lookaddons',
+    'lookhead',
+    'lookbody',
+    'looklegs',
+    'lookfeet',
+    'mount',
+    'resize',
 ];
 
 export function outfitURL({
-	resize,
-	...params
-}: {
-	looktype: number;
-	lookaddons?: number;
-	lookhead?: number;
-	lookbody?: number;
-	looklegs?: number;
-	lookfeet?: number;
-	mount?: number;
-	resize?: boolean;
+                              resize,
+                              ...params
+                          }: {
+    looktype: number;
+    lookaddons?: number;
+    lookhead?: number;
+    lookbody?: number;
+    looklegs?: number;
+    lookfeet?: number;
+    mount?: number;
+    resize?: boolean;
 }): string {
-	const search = new URLSearchParams();
-	for (const [key, value] of Object.entries(params)) {
-		if (!validOutfitKeys.includes(key)) {
-			continue;
-		}
-		search.append(key, (value ?? 0).toString());
-	}
-	if (resize) {
-		search.append('resize', '1');
-	}
-	return `/api/outfits?${search.toString()}`;
+    const search = new URLSearchParams();
+    for (const [key, value] of Object.entries(params)) {
+        if (!validOutfitKeys.includes(key)) {
+            continue;
+        }
+        search.append(key, (value ?? 0).toString());
+    }
+    if (resize) {
+        search.append('resize', '1');
+    }
+    return `/api/outfits?${search.toString()}`;
 }
 
 export function getPronoun(character: {
-	pronoun: PlayerPronoun;
-	name: string;
-	sex: PlayerSex;
+    pronoun: PlayerPronoun;
+    name: string;
+    sex: PlayerSex;
 }) {
-	if (character.pronoun === PlayerPronoun.Name) {
-		return $_('pronouns.name');
-	}
-	const pronoun =
-		character.pronoun != PlayerPronoun.Unset
-			? character.pronoun
-			: character.sex === PlayerSex.Female
-			? PlayerPronoun.She
-			: PlayerPronoun.He;
-	return pronounString(pronoun);
+    if (character.pronoun === PlayerPronoun.Name) {
+        return $_('pronouns.name');
+    }
+    const pronoun =
+        character.pronoun != PlayerPronoun.Unset
+            ? character.pronoun
+            : character.sex === PlayerSex.Female
+                ? PlayerPronoun.She
+                : PlayerPronoun.He;
+    return pronounString(pronoun);
 }
